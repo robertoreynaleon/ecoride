bc99b73bcfd56a4d08e91bd09d3df9197f5a08ef
robertoreynaleon <robertoreynaleon@gmail.com>
2026-06-22 16:44:00 +0200

    feat: styliser le formulaire d'inscription

--- ecoride-front-end/src/main.tsx
+++ ecoride-front-end/src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './styles/global.scss'
 import Home from './Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import Login from './pages/user/Login'
+import Register from './pages/user/Register'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- ecoride-front-end/src/pages/user/Login.tsx
+++ ecoride-front-end/src/pages/user/Login.tsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
 import './Login.scss'
 
 function Login() {
